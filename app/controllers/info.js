5cf2e0a66c157089a77618b83c6281477df9dc94
rasuloff789 <rasuloff789@gmail.com>
2021-05-06 17:05:41 +0500

    add info js

---
+++ app/controllers/info.js
@@ -0,0 +1,42 @@
+const readF = require("util").promisify(require("fs").readFile)
+const writeF = require("util").promisify(require("fs").writeFile)
+const path = require("path").resolve("info.json")
+console.log(path)
+module.exports = {
+  GET: async (req, res) => {
+    try {
+      const infos = JSON.parse(await readF(path), "utf8")
+
+      res.send(infos)
+    } catch (error) {
+      console.log(error)
+      res.send(error)
+    }
+  },
+  POST: async ({
+    body: {
+      address,
+      about,
+      phone_number,
+      email,
+      yt_video_link,
+      google_form_link
+    }
+  }, res) => {
+    try {
+      // authJWT(req)
+      const blogs = await writeF(path, JSON.stringify({
+        address,
+        about,
+        phone_number,
+        email,
+        yt_video_link,
+        google_form_link
+      }))
+
+      res.send(JSON.parse(await readF(path), "utf8"))
+    } catch (error) {
+      res.send(error)
+    }
+  },
+}
