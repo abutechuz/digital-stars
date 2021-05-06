4ed8515b11dbab6d910ec67cfb50e0334bfaf671
Muhammad Siddiq <62147139+codernetic@users.noreply.github.com>
2021-05-06 14:23:35 +0500

    .

--- database/model.sql
+++ database/model.sql
@@ -8,3 +8,29 @@ create table users(
 );
 
 create unique index user_login_idx on users(lower(user_login));
+
+
+create table blogs(
+  blog_id serial not null primary key,
+  blog_title varchar(512) not null,
+  blog_image varchar(4096) not null,
+  blog_content json not null,
+  blog_author varchar(128) not null,
+  blog_author_picture varchar(4096),
+  blog_author_link varchar(4096),
+  blog_reading_time int not null,
+  blog_like int,
+  blog_created TIMESTAMP with time zone default current_timestamp
+);
+
+create TABLE faq(
+  faq_id serial not null primary key,
+  faq_question varchar(512) not null,
+  faq_answer varchar(2048) not null,
+  faq_created  TIMESTAMP with time zone default current_timestamp
+);
+
+create table info(
+  id serial not null primary key,
+  info json not null
+);
