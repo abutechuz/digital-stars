05bf6f6fd72214b152aaac2a777194dbff2e8dee
rasuloff789 <rasuloff789@gmail.com>
2021-05-07 01:33:51 +0500

    auth add user_id define

--- app/Library/function/auth.js
+++ app/Library/function/auth.js
@@ -1,8 +1,25 @@
-const { verify } = require('../jwt.js')
+const {
+  fetch
+} = require('../database/postgres.js')
+const {
+  verify
+} = require('../jwt.js')
 
-module.exports = ({ cookies: { token }}) => {
+module.exports = async ({
+  cookies: {
+    token
+  }
+}) => {
   try {
-    return verify(token)
+    const SQL = `select * from users where user_id =$1`
+    let JWT = await verify(token)
+    if (fetch(SQL, JWT.user_id)) {
+      return JWT
+    } else {
+      throw new Error({
+        message: 'This user is not defined',
+      })
+    }
   } catch (error) {
 
     throw new Error({
