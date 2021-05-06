dc2f61e9858be2456f73568d8d0dc0145614b0f6
Muhammad Siddiq <62147139+codernetic@users.noreply.github.com>
2021-05-06 17:41:30 +0500

    .

--- database/model.sql
+++ database/model.sql
@@ -1,11 +1,14 @@
 create database starter with owner 'postgres' encoding 'utf8';
 create extension pgcrypto;
 
+create domain login as varchar(32) check(value ~* '^@?(\w){4,32}$');
+
 create table users(
   user_id serial not null primary key,
-  user_login VARCHAR(255) not null,
+  user_login login not null,
   user_password varchar(72) not null
 );
+
 
 create unique index user_login_idx on users(lower(user_login));
 
