d8d20d8cc77b2b5824dd809eb8d00858f48ea690
rasuloff789 <rasuloff789@gmail.com>
2021-05-20 15:51:48 +0500

    sile

--- app/controllers/slide.js
+++ app/controllers/slide.js
@@ -9,48 +9,46 @@ module.exports = {
       res.send(error)
     }
   },
-
+  
   POST: async (req, res) => {
     try {
-      let slide_image;
+      let slide_image = req.files.slide_image || {mimetype: ""} ;
       let uploadPath;
-
+      
       const {
         v4: uuidv4
       } = require('uuid');
-      if (!req.files || Object.keys(req.files).length === 0) {
-        return res.status(400).send('No files were uploaded.');
+      if (!req.files || Object.keys(req.files).length === 0 || slide_image.mimetype.match(/image/g)) {
+        return res.status(400).send({message : 'File ERROR'});
       }
-
+      
       // The name of the input field (i.e. "slide_image") is used to retrieve the uploaded file
-
+      
       var path = require('path');
       var scriptName = path.dirname(path.dirname(path.dirname(__filename)));
-
-      slide_image = req.files.slide_image;
-
+      
       let slide_image_src;
-       if (slide_image.mimetype.match(/image/g) && slide_image) {
+      if (slide_image.mimetype.match(/image/g) && slide_image) {
         let val = uuidv4();
         uploadPath = scriptName + '/data/images/' + val;
         slide_image.mv(uploadPath, function (err) {
           if (err)
-            return res.status(500).send(err)
+          return res.status(500).send(err)
         });
         slide_image_src = val
       }
-
+      
       const slide = await slidesModel.insertSlide(req, slide_image_src)
       res.send(slide)
     } catch (error) {
       res.send(error)
     }
   },
-
+  
   DELETE: async (req, res) => {
     try {
       const slide = await slidesModel.deleteSlide(req)
-
+      
       res.send(slide)
     } catch (error) {
       res.send(error)
