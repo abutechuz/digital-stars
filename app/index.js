724feb2c6ec3d971ed82ef2a84a8ce6ec39aca5d
rasuloff789 <rasuloff789@gmail.com>
2021-05-23 15:53:18 +0500

    .

--- app/index.js
+++ app/index.js
@@ -2,23 +2,23 @@ const express = require('express')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const fileUpload = require('express-fileupload')
-const rateLimit = require('express-rate-limit')
-const xss = require('xss-clean')
+// const rateLimit = require('express-rate-limit')
+// const xss = require('xss-clean')
 const helmet = require('helmet')
 const path = require('path')
 
 const app = express()
 const auth = require('./library/function/auth.js')
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-  message: 'Too many requests. Please try after few minutes!'
-})
+// const limiter = rateLimit({
+//   windowMs: 15 * 60 * 1000,
+//   max: 100,
+//   message: 'Too many requests. Please try after few minutes!'
+// })
 
-app.use(xss())
+// app.use(xss())
 app.use(cors({origin:'*'}))
-app.use(helmet())
-app.use(limiter)
+// app.use(helmet())
+// app.use(limiter)
 app.use(express.json({ limit: '10kb' }))
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
