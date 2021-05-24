6d562f16256d07c1c522fc583b36493a8bb97adb
rasuloff789 <rasuloff789@gmail.com>
2021-05-24 09:03:59 +0500

    .

--- app/index.js
+++ app/index.js
@@ -11,7 +11,7 @@ const app = express()
 const auth = require('./library/function/auth.js')
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100,
+  max: 10000,
   message: 'Too many requests. Please try after few minutes!'
 })
 
